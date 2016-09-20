6554cd1416664c95a3d658c92261d936971e6925
Adam Ciołkowski <adamciolkowski1@gmail.com>
2016-09-20 23:17:17 +0200

    Use template string

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -3,7 +3,7 @@ import React, {Component, PropTypes} from 'react';
 export default class HelloWorld extends Component {
 
     render() {
-        var text = this.props.message + ' from React component!';
+        var text = `${this.props.message} from React component!`;
         return <div>{text}</div>;
     }
 }
