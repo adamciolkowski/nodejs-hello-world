d33ce565c837fecbcf33a083ad1c16cc374b9753
Adam Ciołkowski <adamciolkowski1@gmail.com>
2016-10-19 21:36:54 +0200

    Added example class

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import HelloWorld from './components/HelloWorld';
+import Person from './model/Person';
+
+let person = new Person('John', 'Smith');
+let message = `Hi ${person.fullName()}! Greetings `;
 
 ReactDOM.render(
-    <HelloWorld message="Hello world"/>,
+    <HelloWorld message={message}/>,
     document.getElementById('hello-world')
 );
